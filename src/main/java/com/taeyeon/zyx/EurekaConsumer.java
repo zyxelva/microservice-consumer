3732c93d4c3f5cf4904d553d6e8f9b260e4994c2
zyxelva <zyxelva@gmail.com>
2018-09-28 17:38:48 +0800

    增加logback

--- src/main/java/com/taeyeon/zyx/EurekaConsumer.java
+++ src/main/java/com/taeyeon/zyx/EurekaConsumer.java
@@ -1,13 +1,15 @@
 package com.taeyeon.zyx;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class EurekaConsumer {
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(EurekaConsumer.class).web(true).run(args);
+		System.out.println("++++++++++++++++++++++EurekaConsumer Start+++++++++++++++++++++++++");
+		SpringApplication.run(EurekaConsumer.class, args);
+		System.out.println("======================EurekaConsumer Start is Done=========================");
 	}
 }
