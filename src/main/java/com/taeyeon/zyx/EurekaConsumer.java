8aa3263286eedd07a40cfe53185b35c319d362ed
zyxelva <zyxelva@gmail.com>
2018-09-28 16:52:41 +0800

    可用Demo完成

--- src/main/java/com/taeyeon/zyx/EurekaConsumer.java
+++ src/main/java/com/taeyeon/zyx/EurekaConsumer.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class EurekaConsumer {
-
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(EurekaConsumer.class).web(true).run(args);
 	}
--- src/main/java/com/taeyeon/zyx/controller/OrderController.java
+++ src/main/java/com/taeyeon/zyx/controller/OrderController.java
@@ -6,10 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 
@@ -29,67 +26,26 @@ public class OrderController {
 	private RestTemplate restTemplate;
 
 	@Value("${user.url}")
-	private String getUserByIdUrl;
+	private String remoteURL;
 
-	//	@RequestMapping("/user/getById/{userId}")
-	//	public Object getUserById(@PathVariable("userId") String userId){
-	//		System.out.println("++++++++++++++++++++++++++Hello world,"+userId);
-	//		logger.debug("param userId : {}", userId);
-	//
-	//		//String url = "http://localhost:8089/user/getById?id=" + userId;// 硬编码
-	//		String url = getUserByIdUrl + "?id=" + userId;
-	//
-	//		Object result = restTemplate.getForObject(url, Object.class);
-	//		logger.debug("param result : {}", result);
-	//
-	//		return result;
-	//	}
-
-	@RequestMapping("/testG/{name}")
+	@GetMapping("/testHello/{name}")
 	public String testHello(@PathVariable("name")String name) {
 		return "Hello world,"+name;
 	}
 
-	@RequestMapping("/user/getById")
-	public Object getUserById2(@RequestParam("userId") String userId){
-		//System.out.println("2222222222222222222Hello world,"+userId);
-		logger.debug("param userId : {}", userId);
-
-		String url = "http://zyxTaeyeon:8089/student/user/getById?id=" + userId;// 硬编码
-//		String url = getUserByIdUrl + "?id=" + userId;
-
-		String result = restTemplate.getForObject(url, String.class);
-		logger.debug("param result : {}", result);
-
-		return result;
-	}
-
-	@RequestMapping("/user/getByTuid")
-	public Object getByTuid(@RequestParam("tuid") Long tuid){
-		logger.debug("param userId : {}", tuid);
-
-//		String url = "http://localhost:8089/student/user/getByTuid?tuid=" + tuid;// 硬编码
-		String url = getUserByIdUrl + "/" + tuid;
-
-		String result = restTemplate.getForObject(url, String.class);
-		logger.debug("param result : {}", result);
-
-		return result;
-	}
-
-	@RequestMapping("/user/getReportById2")
-	public ResponseEntity<TbClientReportDto> getReportById2(@RequestParam("id") Long id){
-		System.out.println("================this is two=====================");
-		String url = "http://zyxTaeyeon:8089/student/user/getReportById2/" + id;
-		ResponseEntity<TbClientReportDto> tbClientReportDto = restTemplate.getForEntity(url, TbClientReportDto.class);
+	@GetMapping("/user/getReportById")
+	public TbClientReportDto getReportById(@RequestParam("id") Long id){
+		System.out.println("================this is one=====================");
+		String url = remoteURL+"getReportById/" + id;
+		TbClientReportDto tbClientReportDto = restTemplate.getForObject(url, TbClientReportDto.class);
 		return tbClientReportDto;
 	}
 
-	@RequestMapping("/user/getReportById")
-	public TbClientReportDto getReportById(@RequestParam("id") Long id){
-		System.out.println("================this is one=====================");
-		String url = "http://zyxTaeyeon:8089/student/user/getReportById/" + id;
-		TbClientReportDto tbClientReportDto = restTemplate.getForObject(url, TbClientReportDto.class);
+	@GetMapping("/user/getReportById2")
+	public ResponseEntity<TbClientReportDto> getReportById2(@RequestParam("id") Long id){
+		System.out.println("================this is two=====================");
+		String url = remoteURL+"getReportById2/" + id;
+		ResponseEntity<TbClientReportDto> tbClientReportDto = restTemplate.getForEntity(url, TbClientReportDto.class);
 		return tbClientReportDto;
 	}
 
